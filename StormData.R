a2abb7c547ebb3eb2ef137c13d384d1a30b3d386
Ramesh Chellaton <rameshchellaton@rameshs-mbp.lan>
2019-11-13 08:40:37 -0500

    most fatalities by event - commit.

---
+++ StormData.R
@@ -0,0 +1,23 @@
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+
+# stormData <- read.csv("repdata-data-StormData.csv", stringsAsFactors = FALSE)
+
+library(dplyr)
+library(ggplot2)
+
+shortData <- stormData %>% select(6:8,23:28)
+shortData <- shortData %>% mutate(propDamage=PROPDMG * ifelse(PROPDMGEXP == 'K',1000,
+                                                              ifelse(PROPDMGEXP == 'M',1000000,0)))
+
+summEVTYPE <- shortData %>% group_by(EVTYPE)
+
+summEVTYPE <- aggregate(FATALITIES ~ EVTYPE, summEVTYPE, FUN=sum)
+summEVTYPE <- subset(summEVTYPE, FATALITIES > 0)
+
+orderedEVTYPE <- summEVTYPE[order(summEVTYPE$FATALITIES, decreasing=TRUE),]
+orderedEVTYPE <- subset(orderedEVTYPE, FATALITIES > quantile(orderedEVTYPE$FATALITIES,.95))
+
+g <- ggplot(orderedEVTYPE)
+g <- g+geom_point(mapping=aes(y=EVTYPE, x=FATALITIES))
