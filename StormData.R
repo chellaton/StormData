f030f148feff5db1b6460cbcebc2c4bc5431dc4b
Ramesh Chellaton <chellaton@yahoo.com>
2019-11-17 12:09:42 -0500

    Add files via upload

--- StormData.R
+++ StormData.R
@@ -2,13 +2,9 @@ library(dplyr)
 library(ggplot2)
 library(gridExtra)
 
-<<<<<<< HEAD
-# stormData <- read.csv("repdata-data-StormData.csv", stringsAsFactors = FALSE)
 
-=======
 #stormData <- read.csv("repdata_data_StormData.csv", stringsAsFactors = FALSE)
 #
->>>>>>> 6e8890469d68778bac2c91f10c8afe49e9a48fd0
 shortData <- stormData %>% select(6:8,23:28)
 shortData <- stormData %>% 
   mutate(propDamage = case_when (
@@ -25,33 +21,28 @@ shortData <- stormData %>%
 
 summEVTYPE <- shortData %>% group_by(EVTYPE)
 
-<<<<<<< HEAD
-summEVTYPE <- aggregate(FATALITIES ~ EVTYPE, summEVTYPE, FUN=sum)
-
-summEVTYPE <- subset(summEVTYPE, FATALITIES > 0)
-
-orderedEVTYPE <- summEVTYPE[order(summEVTYPE$FATALITIES, decreasing=TRUE),]
-orderedEVTYPE <- subset(orderedEVTYPE, FATALITIES > quantile(orderedEVTYPE$FATALITIES,.95))
-cat("Event with highest fatalities: ") 
-cat(orderedEVTYPE$EVTYPE[1])
-g <- ggplot(orderedEVTYPE)
-g <- g+geom_point(mapping=aes(y=EVTYPE, x=FATALITIES))
-print(g)
-=======
 aggStormData_Fatality <- aggregate(FATALITIES ~ EVTYPE, summEVTYPE, FUN=sum)
 aggStormData_Fatality <- subset(aggStormData_Fatality, FATALITIES > 0)
-aggStormData_Fatality <- aggStormData_Fatality[order(aggStormData_Fatality$FATALITIES, decreasing=TRUE),]
-aggStormData_Fatality <- subset(aggStormData_Fatality, FATALITIES > quantile(aggStormData_Fatality$FATALITIES,.95))
+aggStormData_Fatality <- aggStormData_Fatality[order(aggStormData_Fatality$FATALITIES, 
+                                                     decreasing=TRUE),]
+aggStormData_Fatality <- subset(aggStormData_Fatality, 
+                                FATALITIES > quantile(aggStormData_Fatality$FATALITIES,.95))
+print(paste("Event with highest fatalities: ", aggStormData_Fatality$EVTYPE[1]))
 
 aggStormData_Economic <- aggregate(totalDamage ~ EVTYPE, summEVTYPE, FUN=sum)
 aggStormData_Economic <- subset(aggStormData_Economic, totalDamage > 0)
-aggStormData_Economic <- aggStormData_Economic[order(aggStormData_Economic$totalDamage, decreasing=TRUE),]
+aggStormData_Economic <- aggStormData_Economic[order(aggStormData_Economic$totalDamage, 
+                                                     decreasing=TRUE),]
 
-aggStormData_Economic <- subset(aggStormData_Economic, totalDamage > quantile(aggStormData_Economic$totalDamage, .98))
+aggStormData_Economic <- subset(aggStormData_Economic, 
+                                totalDamage > quantile(aggStormData_Economic$totalDamage, .98))
+print(paste("Event with highest economic loss: ", aggStormData_Economic$EVTYPE[1]))
 
-g1 <- ggplot(aggStormData_Fatality)+geom_point(mapping=aes(y=EVTYPE, x=FATALITIES))
-print(g1)
+g1 <- ggplot() + geom_point(aggStormData_Fatality, mapping=aes(y=EVTYPE, x=FATALITIES), color="dark red") + 
+  ggtitle("Loss of Lives from Natural Disasters") + xlab("Number of Fatalities") + ylab("")
 
-g2 <- ggplot()+geom_point(aggStormData_Economic, mapping=aes(y=EVTYPE, x=totalDamage)) + ylab("")
-print(g2)
->>>>>>> 6e8890469d68778bac2c91f10c8afe49e9a48fd0
+g2 <- ggplot() +
+  geom_point(aggStormData_Economic, mapping=aes(y=EVTYPE, x=totalDamage/1000000), color="dark blue") + 
+  ggtitle("Economic Loss from Natural Disasters") + xlab("Loss in Million $") + ylab("")
+
+grid.arrange(g1, g2, nrow=2)
